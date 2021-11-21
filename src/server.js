35889ac6864ee4e5ee0b352296d87d072f0fd853
Paulo Sergio <pauloserg861@gmail.com>
2021-11-21 19:20:20 -0300

    adicionando estrutura inicial de projeto

---
+++ src/server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+//defining template engine
+app.set('view engine', 'ejs')
+
+
+//setting path from static files
+app.set('views', path.join(__dirname, 'views'))
+
+//definning public files
+app.use(express.static(path.join(__dirname, 'public')))
+
+//habiliting server to receive form data
+app.use(express.urlencoded({ extended: true }))
+
+//defining routs
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Home - PetsShop',
+    })
+})
+
+//open server
+const port = process.env.port || 8085
+app.listen(port, () => console.log(`Server is running at port ${port}`))
