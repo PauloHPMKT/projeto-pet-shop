e83de1f2a0d408508e39a2487e78a43f816d2af1
Paulo Sergio <pauloserg861@gmail.com>
2021-12-19 09:05:44 -0300

    adicionando funcionalidade do formulario

--- src/public/js/toastForm.js
+++ src/public/js/toastForm.js
@@ -1,6 +1,18 @@
 const btnteste = document.querySelector('#testebtn')
+const divTeste = document.querySelector('.teste')
 
 
-btnteste.addEventListener('click', () => {
-   document.querySelector('#teste').classList.add('visible')
-})
+function callForm() {
+   divTeste.classList.add('visible')  
+}
+
+function removeForm(e) {
+   const containerForm = e.target
+   containerForm.classList.remove('visible')
+}
+
+btnteste.addEventListener('click', callForm)
+divTeste.addEventListener('click', removeForm)
+
+
+
